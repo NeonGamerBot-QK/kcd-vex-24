9dad49aef623c697ac5b49ffb18de4b9e6ae7b38
Saahil <neon@saahild.com>
2023-10-11 17:19:58 -0400

    why

--- dash/src/index.js
+++ dash/src/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const c = require('child_process')
+const fs = require('fs')
 const morgan = require('morgan');
+const checkPros = () => {
+    try {
+       c.execSync('pros');
+    } catch (e) {
+        console.log(
+            new Array(10).fill(0).map(() => ' ').join('') 
+        );
+console.log('\nNO PROS INSTALLED\n')
+
+        console.log(
+            new Array(10).fill(0).map(() => ' ').join('') 
+        );
+        process.exit(1)
+   }
+}
+checkPros()
 app.use(morgan('dev'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
---
+++ dash/src/t_/look_for_brains.js
@@ -0,0 +1,14 @@
+const { execSync } = require('child_process')
+
+// Path: dash/src/t_/read_brain_data.js
+// Compare this snippet from dash/src/index.js:
+const rawData = (execSync('pros lsusb').toString().split('\n').filter(Boolean));
+if (rawData.every(e => e.includes('There are no connected'))) {
+    console.log('No brains found');
+    process.exit();
+}
+
+
+
+
+
