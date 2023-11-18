6b6f79e4b75f5dd85341703d3542b7b5d39495c8
Saahil <saahilattud@gmail.com>
2023-11-18 11:33:29 -0500

    feat(intake): intake & config

---
+++ project/src/config.hpp
@@ -0,0 +1,9 @@
+/**
+ * Config
+*/
+#define LEFT_MTR_1_PORT 1 
+#define LEFT_MTR_2_PORT 2 
+#define RIGHT_MTR_1_PORT 3 
+#define RIGHT_MTR_2_PORT 4 
+
+#define INTAKE_PORT 6
---
+++ project/src/intake/intake.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+pros::Motor intake_mtr (6);
+void Moveintake() {
+
+}
+
+void StopIntake() {
+
+}
