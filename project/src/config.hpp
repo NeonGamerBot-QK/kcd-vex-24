2827c36d41135412e7781b3efbc4fcfb3aeb7d54
Saahil <saahilattud@gmail.com>
2023-12-13 16:47:47 -0500

    incomplete

--- project/src/config.hpp
+++ project/src/config.hpp
@@ -1,9 +1,18 @@
+#pragma once
+#include "main.h"
 /**
  * Config
 */
-#define LEFT_MTR_1_PORT 1 
-#define LEFT_MTR_2_PORT 2 
-#define RIGHT_MTR_1_PORT 3 
-#define RIGHT_MTR_2_PORT 4 
+
+#define DRIVETRAIN_PORT_1 1
+#define DRIVETRAIN_PORT_2 3
+#define DRIVETRAIN_PORT_3 2
+#define DRIVETRAIN_PORT_4 4
+
+#define OUTAKE_PORT 7
+#define OUTAKE_PORT_2 8
 
 #define INTAKE_PORT 6
+
+// extern auto pros::E_CONTROLLER_DIGITAL_A;
+// extern pros::controler DIGITAL_A;
