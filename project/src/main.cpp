11c7c1f85541c966078f1f80415f34924e605655
Saahil <saahilattud@gmail.com>
2024-02-13 18:07:07 -0500

    yunis

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -128,10 +128,10 @@ void autonomous() {
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1); // right
-	pros::Motor left_mtr2(4); //left
-	pros::Motor right_mtr(1); // right 
-	pros::Motor right_mtr2(2); //left
+	pros::Motor left_mtr(1); // right CATA
+	pros::Motor left_mtr2(3); // RIGHT B CATA
+	pros::Motor right_mtr(2); // LEFT 
+	pros::Motor right_mtr2(4); //left
 	pros::Motor outtake2(10);
 	
 
@@ -139,8 +139,8 @@ void opcontrol() {
 	//	pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 	//	                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 	//	                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = -master.get_analog(ANALOG_LEFT_Y);
-		int right = -master.get_analog(ANALOG_RIGHT_Y);
+		int left = -master.get_analog(ANALOG_RIGHT_Y);
+		int right = -master.get_analog(ANALOG_LEFT_Y);
 		
 		// std::cout << left_mtr.get_position() << std::endl;
 		// std::cout << 'test log' << std::endl;
